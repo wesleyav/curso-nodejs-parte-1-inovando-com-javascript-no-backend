37532ec767ea8bcf61bd65920e377050575de9cf
Wesley <wesley.contato@hotmail.com>
2020-08-05 07:52:25 -0300

    Organização do projeto

--- CASADOCODIGO/server.js
+++ CASADOCODIGO/server.js
@@ -1,15 +1,32 @@
 const http = require('http');
 
 const servidor = http.createServer(function (req, resp) {
-    resp.end(`
+
+    let html = '';
+    if (req.url == '/') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1>Casa do Código</h1>
+                </body>
+            </html >
+        `;
+    } else if (req.url == '/livros') {
+        html = `
         <html>
             <head>
                 <meta charset="utf-8">
             </head>
             <body>
-                <h1>Casa do Código</h1>
+                <h1>Listagem de livros</h1>
             </body>
         </html >
-    `);
+    `;
+    }
+
+    resp.end(html);
 });
 servidor.listen(3000);
