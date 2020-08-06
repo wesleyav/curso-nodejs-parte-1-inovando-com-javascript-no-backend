b5d63b809994eb4140e42671bc525d573854acc2
Wesley <wesley.contato@hotmail.com>
2020-08-06 20:29:30 -0300

    Edição de livros

--- CASADOCODIGO/src/app/rotas/rotas.js
+++ CASADOCODIGO/src/app/rotas/rotas.js
@@ -32,7 +32,22 @@ module.exports = (app) => {
     });
 
     app.get('/livros/form', function(req, resp) {
-        resp.marko(require('../views/livros/form/form.marko'));
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
+    });
+
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+    
+        livroDao.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    
     });
 
     app.post('/livros', function(req, resp) {
