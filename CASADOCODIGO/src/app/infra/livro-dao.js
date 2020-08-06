37725b31dc83900569b56c566ec80a15615e26f4
Wesley <wesley.contato@hotmail.com>
2020-08-06 08:05:30 -0300

    Persistência e redirecionamento

--- CASADOCODIGO/src/app/infra/livro-dao.js
+++ CASADOCODIGO/src/app/infra/livro-dao.js
@@ -4,6 +4,31 @@ class LivroDao {
         this._db = db;
     }
 
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                    titulo,
+                    preco,
+                    descricao
+                ) values (?,?,?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+                    resolve();
+                }
+            )
+        });
+    }
+
     lista() {
         return new Promise((resolve, reject) => {
             this._db.all(
--- CASADOCODIGO/src/app/rotas/rotas.js
+++ CASADOCODIGO/src/app/rotas/rotas.js
@@ -40,7 +40,7 @@ module.exports = (app) => {
 
         const livroDao = new LivroDao(db);
         livroDao.adiciona(req.body)
-        .then(???)
+        .then(resp.redirect('/livros'))
         .catch(erro => console.log(erro));
     });
 };
