85adc699db21f66e4588e63d9e07a6b495b6e5ea
Wesley <wesley.contato@hotmail.com>
2020-08-06 06:54:56 -0300

    DAO com Promises

--- CASADOCODIGO/src/app/rotas/rotas.js
+++ CASADOCODIGO/src/app/rotas/rotas.js
@@ -21,16 +21,24 @@ module.exports = (app) => {
     app.get('/livros', function (req, resp) {
 
         const livroDao = new LivroDao(db);
+        livroDao.lista()
+        .then(livros => resp.marko(
+            require('../views/livros/lista/lista.marko'),
+            {
+                livros: livros
+            }
+        ))
+        .catch(erro => console.log(erro));
 
-        livroDao.lista(function(erro, resultados){
+        // livroDao.lista(function(erro, resultados){
 
-            resp.marko(
-                require('../views/livros/lista/lista.marko'),
-                {
-                    livros: resultados
-                }
-            );
-        });
+        //     resp.marko(
+        //         require('../views/livros/lista/lista.marko'),
+        //         {
+        //             livros: resultados
+        //         }
+        //     );
+        // });
     });
 };
 
